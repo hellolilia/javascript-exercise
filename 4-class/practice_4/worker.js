01f9750ebfb96af32a077151ccc0591f69cd4d37
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-09-21 23:13:39 +0800

    completed class practice_4 test

--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,11 @@
-// TODO 17: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  introduce() {
+    return `${this.basicIntroduce()} I am a Worker. I have a job.`;
+  }
+}
