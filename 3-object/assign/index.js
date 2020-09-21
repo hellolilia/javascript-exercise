200242ca863874c2e7c509309734b71d10627a4b
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-09-21 22:02:52 +0800

    completed assign test

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,4 @@
 export default function addSerialNumber(source) {
-  // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  source.properties.status = 'processed';
+  return Object.assign({ serialNumber: '12345' }, source);
 }
